b6eb0beaa755818d55981adb6f1613ec9ba1936b
Change-37 <202110305@bssm.hs.kr>
2021-12-14 10:48:40 +0900

    2021.12.13.

---
+++ storeManager.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Quick(int list[], int left, int right){
+
+}
+
+void QuSort(int list[], int start, int end){
+
+}
+
+void Manager(int list[]){
+    
+}
+
+void main(){
+    int n;
+
+}
